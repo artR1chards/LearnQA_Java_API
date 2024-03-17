b77a4a2418bd649f0a943dbbb570cb01b0c1bc3c
arozhkov@htc-cs.ru <arozhkov@htc-cs.ru>
2024-03-17 17:24:18 +0400

    Ex6: Редирект

--- src/test/java/testGetResponse.java
+++ src/test/java/testGetResponse.java
@@ -1,15 +1,20 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class testGetResponse {
     @Test
     public void testGetResponseFromTestSite()
     {
-        JsonPath response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_json_homework")
-                .jsonPath();
-        String secondMessage = response.get("messages[1].message");
-        System.out.println(secondMessage);
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String headerLocation = response.getHeader("Location");
+        System.out.println("Location is " + headerLocation);
     }
 }
